8b19cef3be820d33149ccdcfcafd974ee70d5fe9
mangri <112893833+mangri@users.noreply.github.com>
2022-10-18 21:37:15 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,22 @@ namespace Order_Management_System
     {
         static void Main(string[] args)
         {
+            while(true)
+            {
+                AskingUserForInput();
+            }
+            Console.WriteLine("Chose one of the following options:");
+            Console.WriteLine("[1] Add new client");
+            Console.WriteLine("[2] Place new order");
+            Console.WriteLine("[3] Update the stock");
+            Console.WriteLine("[4] Get reports");
+            Console.WriteLine("[5] Exit");
+            // Chose one of the following options
+            // switch for: Add new client, Place new order, Update the stock, Get reports (new file for each report), Exit.
+            // Another request? y/n
+            // each of them has a method
+            // place excel files in a root folder for Visual Studio to seee those
+            
             List<Clientele> clients = new List<Clientele>();
             string path = @"C:\Users\mangri\source\repos\OrderManagementSystem\Order_Management_System\bin\Debug\net5.0\";
             string fileNameClients = "Clientele.xlsx";
